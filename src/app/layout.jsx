f252d930cbca47654cafb1703558376a804b40d1
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2025-04-15 10:25:11 -0300

    feat: create project api

---
+++ src/app/layout.jsx
@@ -0,0 +1,27 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Atividade Consumo API",
+  description: "Atividade sobre consumo APIs sobre Gerador de User Aleatório",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
